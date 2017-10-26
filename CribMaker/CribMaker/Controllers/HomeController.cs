d965a64da7f805b9fd136ec1dcfd1b1ecb663b03
Oleh Vintoniak <mr.mr.oleh.20@gmail.com>
2017-10-26 09:55:02 +0300

    updated packages

--- CribMaker/CribMaker/Controllers/HomeController.cs
+++ CribMaker/CribMaker/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using CribMaker.Core.Data;
+using CribMaker.Core.Repositories.Abstract;
+using CribMaker.Core.Repositories.Factory;
+using CribMaker.Services.Services;
+using CribMaker.Services.Services.Factory;
 
 namespace CribMaker.Controllers
 {
     public class HomeController : Controller
     {
+        private IApplicationUserService _userService;
+
+        public HomeController()
+        {
+            var context = ApplicationDbContext.Create();
+            var uow = new UnitOfWork(context);
+            var repositoryManager = new RepositoryManager(context);
+            var sm = new ServiceManager(uow,repositoryManager);
+            _userService = sm.ApplicationUserService;
+        }
+
         public ActionResult Index()
         {
+            var i = _userService.GetByUserName(User.Identity.Name);
             return View();
         }
 
